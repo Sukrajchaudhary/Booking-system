570d71e3bde4fd7918e6a5b330a152aefc16f822
Sukrajchaudhary <sukrajchaudhary90@gmail.com>
2023-09-08 21:41:59 +0545

    first commit

--- controller/auth.js
+++ controller/auth.js
@@ -3,8 +3,14 @@ const crypto = require("crypto");
 const { sanitizer } = require("../services/common");
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = process.env.SECRET_KEY;
+
 exports.createUsers = async (req, res) => {
   try {
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User  with these email already exists !" });
+    }
+
     const salt = crypto.randomBytes(16);
     crypto.pbkdf2(
       req.body.password,
@@ -13,11 +19,16 @@ exports.createUsers = async (req, res) => {
       32,
       "sha256",
       async function (err, hashedPassword) {
+        if (err) {
+          return res.status(400).json({ message: "Password hashing failed" });
+        }
+
         const user = new User({
           ...req.body,
           password: hashedPassword,
           salt: salt,
         });
+
         const response = await user.save();
         req.login(sanitizer(user), (err) => {
           if (err) {
